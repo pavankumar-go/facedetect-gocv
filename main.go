23f1cf5ffab6530951af4830de90fd9852ebd0c7
pavan.k <pavan.k@digitalapicraft.com>
2020-07-28 20:20:34 +0530

    FDGOCV - main.go - fixes function comments

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 const haarClassifier = "haar_classifier_face.xml"
 const fontFace = gocv.FontHersheyPlain
 
+// properties for font and rectangle
 var (
 	fontSize      = 2.0
 	fontThickness = 3
@@ -22,6 +23,7 @@ var (
 )
 
 func main() {
+	// address of the machinebox running on your local
 	machineboxClient := facebox.New("http://localhost:8080")
 
 	// Initialize Webcam
@@ -36,66 +38,65 @@ func main() {
 	window := gocv.NewWindow("Face Detection")
 	defer window.Close()
 
-	// returns image matrix
+	// returns image matrix to which your images will be written
 	imgMat := gocv.NewMat()
 	defer imgMat.Close()
 
-	// initializes classifier
+	// initializes cascade classifier
 	classifer := gocv.NewCascadeClassifier()
-	classifer.Load(haarClassifier) // using HAAR_face_default algorithm
+	classifer.Load(haarClassifier) // loads Haar cascade classifier
 	defer classifer.Close()
 
 	for {
-		// reads image from webcam and read it to image matrix
+		// reads image from the video frame and writes it to image matrix
 		if ok := webcam.Read(&imgMat); !ok || imgMat.Empty() {
 			log.Print("Faild to read image from webcam")
 			continue
 		}
+
 		// initially we dont the person in the image
-		// Before training
 		faceName := "Unknown Person"
 
-		// image detection using haar classifier
+		// face detection using haar classifier from the image matrix
 		imgRects := classifer.DetectMultiScale(imgMat)
 		for _, imgRect := range imgRects {
-
 			imgFace := imgMat.Region(imgRect)
 
-			/* IMWrite is used to capture current image of a person being read from webcam
-			commented after getting the images
-			uncomment and run it to get your images captured as its being displayed in webcam
-			*/
-
-			// if ok := gocv.IMWrite(fmt.Sprintf("pavan-%d.jpg", time.Now().Unix()), imgMat); !ok {
+			// IMWrite is used to capture current image of a person being read from webcam
+			// commented after getting the images
+			// uncomment and rerun it to get your images captured and
+			// saved in the current directory as its being displayed in webcam
+			// can be used for image training in your machinebox
+			// if ok := gocv.IMWrite(fmt.Sprintf("imagename-%d.jpg", time.Now().Unix()), imgMat); !ok {
 			// 	log.Print("Failed to write image")
 			// }
 
-			// IMEncode stores in mem buffer
-			// can be read from buffer later
+			// IMEncode stores image matrix in mem buffer
+			// will be read from buffer later
 			imgBuf, err := gocv.IMEncode(".jpg", imgMat)
 			if err != nil {
 				log.Print("failed to encode image", err)
 			}
 			imgFace.Close()
 
-			// uses the image buffer to check with the trained image using machine-box client
+			// reads from the buffer to match with the image trained in machinebox's facebox
 			faces, err := machineboxClient.Check(bytes.NewBuffer(imgBuf))
 			if err != nil {
 				log.Println("failed to check image: ", err)
 			}
 			if len(faces) > 0 {
-				faceName = faces[0].Name // if there is face i.e., matched we show that name
+				faceName = faces[0].Name // name of the matched face/image
 			}
-
-			textSize := gocv.GetTextSize(faceName, fontFace, fontSize, fontThickness) // configure text-properties to display
+			// configure image text properties to display
+			textSize := gocv.GetTextSize(faceName, fontFace, fontSize, fontThickness)
 			imgPt := image.Pt(imgRect.Min.X+(imgRect.Min.X/2)-(textSize.X/2), imgRect.Min.Y-2)
-
-			gocv.PutText(&imgMat, faceName, imgPt, fontFace, fontSize, fontColor, fontThickness) // displays text for a image
-			gocv.Rectangle(&imgMat, imgRect, rectColor, rectThickness)                           // draws rectangle around the face
-
+			// displays text for an face/image
+			gocv.PutText(&imgMat, faceName, imgPt, fontFace, fontSize, fontColor, fontThickness)
+			// draws rectangle around the face/image
+			gocv.Rectangle(&imgMat, imgRect, rectColor, rectThickness)
 		}
 
-		window.IMShow(imgMat) // displays the face on screen
+		window.IMShow(imgMat) // displays the face/image on screen
 		window.WaitKey(1)     // must be called for image processing
 	}
 }
